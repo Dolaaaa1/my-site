b398b10dd4c5bff540d001f4eef655d479f4edbe
احمد عادل شعبان <ahmedadel@MacBook-Pro-ahmd.local>
2023-10-23 06:37:28 +0300

    Editing views file and html files

--- src/blog/views.py
+++ src/blog/views.py
@@ -44,7 +44,8 @@ class SinglePostView(View):
         context ={
             "post":post,
             "post_tags":post.tags.all(),
-            "comment_form": CommentForm()
+            "comment_form": CommentForm(),
+            "comments":post.comments.all().order_by("-id")
             
         }
         return render(request,"blog/post-detail.html",context)
@@ -62,7 +63,8 @@ class SinglePostView(View):
         context ={
             "post":post,
             "post_tags":post.tags.all(),
-            "comment_form": CommentForm()
+            "comment_form": CommentForm(),
+            "comments":post.comments.all().order_by("-id") 
         }    
         return render(request,"blog/post-detail.html",context)
     
